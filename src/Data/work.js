dd810780776d0e48ab328e439f268edc43936b91
rromikas <rromikas@gmail.com>
2021-04-15 19:07:33 +0300

    ok

--- src/Data/work.js
+++ src/Data/work.js
@@ -1,23 +1,36 @@
 export const workData = [
-    {
-        title : "Customize your card ", option : [
-            { subtitle : "Select 1/- card variants, Fill out the customization form and personalize the card the with interactive display "}
-        ]
-    },
-    {
-        title : "Complete purchase at checkout"
-    },
-    {
-        title : "Ship your card to us"
-    },
-    {
-        title : "We receive your card", option : [
-            { subtitle : "In 2 – 3 days you will be emailed regarding us having received your package containing your plastic debit /credit card(s)."}
-        ]
-    },
-    {
-        title : "We ship your new card to you" , option : [
-            { subtitle : "We will ship both your old plastic and your new METAL cards back to you in our luxurious packaging for FREE."}
-        ]
-    }
-]
+  {
+    title: "Customize your card ",
+    option: [
+      {
+        subtitle:
+          "Select your card variant, fill out the customization form and personalize the card with interactive web application.",
+      },
+    ],
+  },
+  {
+    title: "Complete purchase at checkout",
+  },
+  {
+    title: "Ship your card to us",
+    option: [{ subtitle: "‘Temporarily Freeze’ your card for safety measures and Ship it to us." }],
+  },
+  {
+    title: "We receive your card",
+    option: [
+      {
+        subtitle:
+          "In 2 - 3 days you will receive an email confirming that we have received your debit/credit card(s).",
+      },
+    ],
+  },
+  {
+    title: "We ship your new card to you",
+    option: [
+      {
+        subtitle:
+          "We will ship both your old plastic and your new METAL cards back to you in our luxurious packaging.",
+      },
+    ],
+  },
+];
